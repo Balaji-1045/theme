024957865bb9fee2aa29f32620f6eea00e069cc7
Balaji-1045 <b43316265@gmail.com>
2025-03-19 16:17:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+var body =document.querySelector("body");
+function dark_theme(){
+    body.className="";
+    body.classList.add("dark");
+}
+
+function light_theme(){
+    body.className="";
+    body.classList.add("light");
+}
